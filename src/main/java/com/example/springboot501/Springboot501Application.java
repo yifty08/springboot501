fc6fcf0cc3d203f3e12bcb6f192fed5547803576
yifty08 <yifty08@icloud.com>
2019-03-14 09:41:28 -0400

    initial commit

---
+++ src/main/java/com/example/springboot501/Springboot501Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot501;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot501Application.class, args);
+	}
+
+}
